c44244069dfb66c163e11379d5f7b279bce2e202
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-05-06 15:43:32 +0800

    fix test

--- packages/babel-node/test/node-flag.js
+++ packages/babel-node/test/node-flag.js
@@ -1,14 +1,18 @@
 import getV8Flags from "v8flags";
 
 getV8Flags(async function (err, v8Flags) {
-  for (const v of v8Flags) {
-    if (!process.allowedNodeEnvironmentFlags.has(v)) {
-      throw Error(
-        v,
-        JSON.stringify(v8Flags) +
-          "\n" +
-          JSON.stringify(process.allowedNodeEnvironmentFlags),
-      );
+  const allowedNodeEnvironmentFlags = process.allowedNodeEnvironmentFlags;
+  console.log(allowedNodeEnvironmentFlags, v8Flags);
+  expect(() => {
+    for (const v of v8Flags) {
+      if (!allowedNodeEnvironmentFlags.has(v)) {
+        throw Error(
+          v,
+          JSON.stringify(v8Flags) +
+            "\n" +
+            JSON.stringify(allowedNodeEnvironmentFlags),
+        );
+      }
     }
-  }
+  }).not.toThrow();
 });
