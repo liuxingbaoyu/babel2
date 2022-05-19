30d21de08ea2842f9ed0e06d03989de49a165e32
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-05-20 06:53:14 +0800

    test

--- jest.setup.js
+++ jest.setup.js
@@ -1,12 +1,25 @@
+const v8 = require("v8");
+const os = require("os");
+
 module.exports = async function (globalConfig, projectConfig) {
+  //projectConfig.cache = false;
+
   console.log(globalConfig.testPathPattern);
   console.log(projectConfig.cache);
 
   setInterval(() => {
     console.log(
-      "#memory",
-      process.pid,
-      process.memoryUsage().rss / 1024 / 1024
+      `#debug: pid=${process.pid}, rss=${
+        process.memoryUsage().rss / 1024 / 1024
+      }, totalRam=${os.totalmem() / 1024 / 1024}, freeRam=${
+        os.freemem() / 1024 / 1024
+      }`
     );
   }, 500).unref();
+  // const aaa = setInterval(() => {
+  //   if (process.memoryUsage().rss / 1024 / 1024 > 500) {
+  //     //clearInterval(aaa);
+  //     //v8.writeHeapSnapshot();
+  //   }
+  // }, 10000);
 };
