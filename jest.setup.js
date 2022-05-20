ac461c16a9161a7b267fd526676473eaa3d4b2d9
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2022-05-20 21:15:06 +0800

    test

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,6 @@
-const v8 = require("v8");
+// const v8 = require("v8");
 const os = require("os");
+const child_process = require("child_process");
 
 module.exports = async function (globalConfig, projectConfig) {
   //projectConfig.cache = false;
@@ -9,13 +10,27 @@ module.exports = async function (globalConfig, projectConfig) {
 
   setInterval(() => {
     console.log(
-      `#debug: pid=${process.pid}, rss=${
+      `#debug-memory: pid=${process.pid}, rss=${
         process.memoryUsage().rss / 1024 / 1024
       }, totalRam=${os.totalmem() / 1024 / 1024}, freeRam=${
         os.freemem() / 1024 / 1024
       }`
     );
   }, 500).unref();
+  if (os.platform() == "linux") {
+    setInterval(() => {
+      try {
+        child_process.exec("df -h", function (err, stdout, stderr) {
+          console.log(
+            `#debug-disk: err=${err}, stdout=${stdout}, stderr=${stderr}`
+          );
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }, 5000).unref();
+  }
+
   // const aaa = setInterval(() => {
   //   if (process.memoryUsage().rss / 1024 / 1024 > 500) {
   //     //clearInterval(aaa);
